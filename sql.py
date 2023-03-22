b07332ed3d7ad7d07dc47a651b6d1552c7529db3
jenna <jenna_martin1@baylor.edu>
2023-03-22 13:56:51 -0500

    class

--- sql.py
+++ sql.py
@@ -28,7 +28,7 @@ class myGUI:
         self.prompt_password.pack(side='left')
         self.password_entry.pack(side='left')
 
-        self.login_button = t.Button(self.frame2,text='Login')
+        self.login_button = t.Button(self.frame2,text='Login', command= self.access_database())
 
         self.login_button.pack()
 
@@ -36,5 +36,44 @@ class myGUI:
         print('helllo')
 
         t.mainloop()
+        login = self.email_entry.get()
+        password= self.password_entry.get()
 
+        self.main_window.destroy()
+
+        login = 'Jenna_martin1'
+        password=''
+
+        prelist= {}
+        courselist= []
+        cn_str =(
+        'Driver=(SQL Server);' # Data source driver,
+        'Server=MIS-SQLJB;'    # Server name
+        'Database=School;'      #Database name
+        'UID='+login+';'        #User name
+        'PWD'+password+';'      #User password
+            )
+
+
+        cn= pyodbc.connect(cn_str)
+
+        cursor = cn.cursor()
+
+        cursor.execute('select * from School.dbo.Course')
+
+        data = cursor.fetchall()
+        print(data)
+        inputcourseid=int(input("What is the course ID"))
+        for row in data:
+            courseID=row[0]
+            title= row[1]
+            credit=row[2]
+            deptID=row[3]
+            prelist = {'CourseID':courseID,'Title': title, 'Credit':credit, 'DeptID':deptID}
+            courselist.append(prelist)
+        for i in courselist
+        if inputcourseid== i[0]:
+            print(f'Title:{i["Title"]}')
+            print(f'Credits:{i["Credit"]}')
+            print(f'Department:{i["DeptID"]}')
 myinstance=myGUI()
